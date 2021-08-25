30fddc4e83fe76d0cf879c8552b831eaac2b3e49
Leah Brenny <leahbrenny@Leahs-MacBook-Pro.local>
2021-08-24 23:32:27 -0500

    added seo tags

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,6 @@ module.exports = {
         title: 'Frontend Masters Intro to Gatsby',
         description: 'Frontend Masters Intro to Gatsby course projects',
         image: 'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
-    }
+    },
+    plugins: ['gatsby-plugin-react-helmet'],
 };
