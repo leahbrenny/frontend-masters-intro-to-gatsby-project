1132a72148b51ce899db19c402571079c4476861
Leah Brenny <leahbrenny@Leahs-MacBook-Pro.local>
2021-08-25 16:14:35 -0500

    added a gatsbyimage to a JS and MDX file

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,28 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout.js';
 
-export default function AboutPage() {
+export const query = graphql`
+query CocktailQuery {
+  file(name: {eq: "cocktail"}) {
+    childImageSharp {
+      gatsbyImageData(placeholder: DOMINANT_COLOR)
+    }
+  }
+}
+`;
+
+export default function AboutPage({ data }) {
   return (
     <Layout
       title="About This Site"
       description="More information abou this site."
     >
+      <GatsbyImage
+      image={getImage(data.file)}
+      alt="a cocktail set in a floral arrangement with dry ice mist around it"
+      />
       <h1>About this site</h1>
       <Link to="/">Back to Home</Link>
     </Layout>
