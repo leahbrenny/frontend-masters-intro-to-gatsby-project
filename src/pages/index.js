75c7daf0f14c5bfe549c737d7cb711cfe06eab2e
Leah Brenny <leahbrenny@Leahs-MacBook-Pro.local>
2021-08-25 17:45:48 -0500

    added an episode list to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,35 +19,65 @@ export default function IndexPage() {
           }
         }
       }
+      allSanityEpisode(
+        sort: { fields: date, order: DESC }
+        filter: { youtubeID: { ne: null } }
+        limit: 20
+      ) {
+        nodes {
+          id
+          title
+          guest {
+            name
+          }
+          gatsbyPath(filePath: "/episode/{SanityEpisode.slug__current}")
+        }
+      }
     }
   `);
 
   const posts = data.allMdx.nodes;
+  const episodes = data.allSanityEpisode.nodes;
 
   return (
     <Layout>
-        <div className={imageWrapper}>
-            <StaticImage
-            src="../images/ivana-la-61jg6zviI7I-unsplash.jpg"
-            alt="A corgi sitting on a bed with red paper hearts all over it. It looks unamused"
-            placeholder="dominantColor"
-            width={300}
-            height={300}
-            />
-        </div>
+      <div className={imageWrapper}>
+        <StaticImage
+          src="../images/ivana-la-61jg6zviI7I-unsplash.jpg"
+          alt="A corgi sitting on a bed with red paper hearts all over it. It looks unamused"
+          placeholder="dominantColor"
+          width={300}
+          height={300}
+        />
+      </div>
 
       <h1>Hello Frontend Masters</h1>
       <Link to="/about">About this site</Link>
 
       <h2>Check out my recent blog posts</h2>
       <ul>
-          {posts.map((post) => (
-              <li key={post.id}>
-                  <Link to={post.slug}>{post.frontmatter.title}</Link>{' '}
-                  <small>posted {post.frontmatter.date}</small>
-              </li>
-          ))}
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={post.slug}>{post.frontmatter.title}</Link>{' '}
+            <small>posted {post.frontmatter.date}</small>
+          </li>
+        ))}
       </ul>
+
+      <h2>Latest Episodes of <em>Learn With Jason</em>
+      </h2>
+      <ul>
+        {episodes.map((episode) => (
+          <li key={episode.id}>
+            <Link to={episode.gatsbyPath}>
+              {episode.title} (with {episode.guest?.[0]?.name})
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <a href="https://www.learnwithjason.dev/">
+        Watch all episodes of <em>Learn With Jason</em>
+      </a>
     </Layout>
   );
 }
